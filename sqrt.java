55fc93badb4c9f30497b1990d5023efee617d671
jlin03 <jlin03@stuy.edu>
2019-01-30 16:29:59 -0500

    small edits

--- sqrt.java
+++ sqrt.java
@@ -8,7 +8,7 @@ public class sqrt{
 			return 0;
 		}
 		if(n < 0) {
-			return null;
+			return sqrtHelper(-n,-guess);
 		}
 		if(Math.abs((guess * guess) - n) > 0.00001) {
 			return sqrtHelper(n,((n / guess) + guess) / 2);
@@ -20,6 +20,8 @@ public class sqrt{
 	
 	public static void main(String[] args) {
 		System.out.println(sqrt(2));
+		System.out.println(sqrt(0));
+		System.out.println(sqrt(-1));
 	}
 	
 	
